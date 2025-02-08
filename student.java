779b1530402dca13efa4904664086922a9d49051
ccm <minhson01111994@gmail.com>
2025-02-08 17:20:43 +0900

    test1 commit

--- student.java
+++ student.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class student {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        int a,b;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập giá trị của a");
+        a = sc.nextInt();
+        System.out.println("Nhập giá trị của b");
+        b = sc.nextInt();
+
+        int sum = a+b;
+
+        System.out.println("Tổng của a và b là:" +sum);
+    }
 }
